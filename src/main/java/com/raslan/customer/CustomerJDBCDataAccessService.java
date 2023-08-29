4bbce4f86ddd64d9daf6863c79816992758a1d43
ahmed <ahmedraslan28@gmail.com>
2023-08-29 23:45:19 +0300

    create updateCustomer and deleteCustomer using jdbc template

--- src/main/java/com/raslan/customer/CustomerJDBCDataAccessService.java
+++ src/main/java/com/raslan/customer/CustomerJDBCDataAccessService.java
@@ -51,12 +51,41 @@ public class CustomerJDBCDataAccessService implements CustomerDAO {
 
     @Override
     public void deleteCustomer(Integer id) {
-        customerRepository.deleteById(id);
+        var sql = """
+                    delete from customer where id = ?
+                """;
+        jdbcTemplate.update(sql, id) ;
     }
 
     @Override
     public void updateCustomer(Customer customer) {
-        customerRepository.save(customer);
+        if (customer.getName() != null) {
+            String sql = "UPDATE customer SET name = ? WHERE id = ?";
+            int result = jdbcTemplate.update(
+                    sql,
+                    customer.getName(),
+                    customer.getId()
+            );
+            System.out.println("update customer name result = " + result);
+        }
+        if (customer.getAge() != null) {
+            String sql = "UPDATE customer SET age = ? WHERE id = ?";
+            int result = jdbcTemplate.update(
+                    sql,
+                    customer.getAge(),
+                    customer.getId()
+            );
+            System.out.println("update customer age result = " + result);
+        }
+        if (customer.getEmail() != null) {
+            String sql = "UPDATE customer SET email = ? WHERE id = ?";
+            int result = jdbcTemplate.update(
+                    sql,
+                    customer.getEmail(),
+                    customer.getId());
+            System.out.println("update customer email result = " + result);
+        }
+
     }
 
     @Override
