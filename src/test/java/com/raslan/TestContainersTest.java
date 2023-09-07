a5bd9cef4b02b3bfad97773bebd7b2f443999ffe
ahmed <ahmedraslan28@gmail.com>
2023-09-07 20:19:16 +0300

    AbstractTestContainer

--- src/test/java/com/raslan/TestContainersTest.java
+++ src/test/java/com/raslan/TestContainersTest.java
@@ -1,55 +1,15 @@
 package com.raslan;
 
-import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
-public class TestContainersTest {
-
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres:latest")
-                    .withDatabaseName("raslan-dao-unit-test")
-                    .withUsername("ahmedraslan")
-                    .withPassword("admin");
-
+public class TestContainersTest extends TestContainersAbstract {
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
-    }
-
-    @DynamicPropertySource
-    private static void registerDataSourceProperty(DynamicPropertyRegistry registry){
-        registry.add(
-                "spring.datasource.url",
-                postgreSQLContainer::getJdbcUrl
-        );
-        registry.add(
-                "spring.datasource.username",
-                postgreSQLContainer::getUsername
-        );
-        registry.add(
-                "spring.datasource.password",
-                postgreSQLContainer::getPassword
-        );
-    }
-
-    @Test
-    void TestFlyWay() {
-        Flyway flyway = Flyway.configure().dataSource(
-                postgreSQLContainer.getJdbcUrl(),
-                postgreSQLContainer.getUsername(),
-                postgreSQLContainer.getPassword()
-        ).load();
-        flyway.migrate();
-        System.out.println();
     }
 }
