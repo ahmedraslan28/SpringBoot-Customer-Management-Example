723d0f46bcf3ec2626c5a5337dc96ab34bda4267
ahmed <ahmedraslan28@gmail.com>
2023-09-22 04:05:12 +0300

    test CD

--- backend/src/main/java/com/raslan/PingPongController.java
+++ backend/src/main/java/com/raslan/PingPongController.java
@@ -5,15 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
-
     private static int COUNTER = 0;
 
-    record PingPong(String result){
-
-    }
+    record PingPong(String result){ }
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("Pong: %s".formatted(++COUNTER));
+        return new PingPong("Pong is: %s".formatted(++COUNTER));
     }
 }
