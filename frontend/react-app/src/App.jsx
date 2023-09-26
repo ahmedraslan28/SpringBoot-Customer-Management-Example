0232ef6fc648d8bb40ae61aa7f51cf921d3aa017
ahmed <ahmedraslan28@gmail.com>
2023-09-26 22:53:45 +0300

    Cors, Axios, Fetch Customers & Card component

---
+++ frontend/react-app/src/App.jsx
@@ -0,0 +1,57 @@
+import { Spinner, Text, Wrap, WrapItem, Center } from "@chakra-ui/react";
+import SidebarWithHeader from "./shared/SideBar";
+import { useEffect, useState } from "react";
+import { getCustomers } from "./services/client";
+import CardWithImage from "./components/Card";
+const App = () => {
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getCustomers()
+      .then((res) => {
+        console.log(res);
+        setCustomers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <SidebarWithHeader>
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      </SidebarWithHeader>
+    );
+  }
+  if (!customers.length) {
+    return (
+      <SidebarWithHeader>
+        <Text>No customers available</Text>
+      </SidebarWithHeader>
+    );
+  }
+  return (
+    <SidebarWithHeader>
+      <Wrap spacing={"50px"} justify={"left"}>
+        {customers.map((customer, index) => (
+          <WrapItem>
+            <CardWithImage {...customer} />
+          </WrapItem>
+        ))}
+      </Wrap>
+    </SidebarWithHeader>
+  );
+};
+
+export default App;
