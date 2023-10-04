dd06b5896ea5ad22cb22a572636a81278e570311
ahmed <ahmedraslan28@gmail.com>
2023-10-04 21:34:09 +0300

    frontend cd

--- frontend/react-app/src/services/client.js
+++ frontend/react-app/src/services/client.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-
+const { REACT_APP_API_BASE_URL } = process.env;
 export const getCustomers = async () => {
   try {
-    return await axios.get(
-      `${process.env.REACT_APP_API_BASE_URL}/api/v1/customers`
-    );
+    return await axios.get(`${REACT_APP_API_BASE_URL}/api/v1/customers`);
   } catch (error) {
     throw error;
   }
@@ -13,30 +11,28 @@ export const getCustomers = async () => {
 export const saveCustomer = async (customer) => {
   try {
     return await axios.post(
-      `${process.env.REACT_APP_API_BASE_URL}/api/v1/customers`,
+      `${REACT_APP_API_BASE_URL}/api/v1/customers`,
       customer
     );
   } catch (error) {
     throw error;
   }
 };
-
 
 export const deleteCustomer = async (id) => {
   try {
     return await axios.delete(
-      `${process.env.REACT_APP_API_BASE_URL}/api/v1/customers/${id}`
+      `${REACT_APP_API_BASE_URL}/api/v1/customers/${id}`
     );
   } catch (error) {
     throw error;
   }
 };
 
-
-export const updateCustomer = async (customer,id) => {
+export const updateCustomer = async (customer, id) => {
   try {
     return await axios.put(
-      `${process.env.REACT_APP_API_BASE_URL}/api/v1/customers/${id}`,
+      `${REACT_APP_API_BASE_URL}/api/v1/customers/${id}`,
       customer
     );
   } catch (error) {
@@ -44,8 +40,7 @@ export const updateCustomer = async (customer,id) => {
   }
 };
 
-/*
-http://customer-react-env.eba-yk5r2usj.me-south-1.elasticbeanstalk.com/customer-api-env.eba-yk5r2usj.me-south-1.elasticbeanstalk.com/api/v1/customer-api-env.eba-yk5r2usj.me-south-1.elasticbeanstalk.com/api/v1/customers
 
-api_base_url=customer-api-env.eba-yk5r2usj.me-south-1.elasticbeanstalk.com
+/*
+http://customer-api-env.eba-yk5r2usj.me-south-1.elasticbeanstalk.com
 */
