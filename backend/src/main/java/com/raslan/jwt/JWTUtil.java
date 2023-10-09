8f9bc7312d07a37eb62da42bff7ecf34ac65e430
ahmed <ahmedraslan28@gmail.com>
2023-10-09 18:39:41 +0300

    create JWTAuthenticationFilter

--- backend/src/main/java/com/raslan/jwt/JWTUtil.java
+++ backend/src/main/java/com/raslan/jwt/JWTUtil.java
@@ -1,8 +1,10 @@
 package com.raslan.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.MacAlgorithm;
+import io.jsonwebtoken.security.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,8 @@ public class JWTUtil {
     @Value("${JWT_SECRET_KEY}")
     private String SECRET_KEY ;
 
+    private static final MacAlgorithm algorithm = Jwts.SIG.HS256 ;
+
 
 
     public String issueToken(String subject){
@@ -35,13 +39,38 @@ public class JWTUtil {
     public String issueToken(String subject, Map<String, Object> claims){
         return Jwts
                 .builder()
-                .setClaims(claims)
-                .setSubject(subject)
-                .setIssuer("reso.com")
-                .setIssuedAt(Date.from(Instant.now()))
-                .setExpiration(Date.from(Instant.now().plus(15, DAYS)))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .claims(claims)
+                .subject(subject)
+                .issuer("reso.com")
+                .issuedAt(Date.from(Instant.now()))
+                .expiration(Date.from(Instant.now().plus(15, DAYS)))
+                .signWith(getSigningKey(), algorithm)
                 .compact();
+    }
+
+    public Claims getClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getSubject(String token){
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token){
+        Date today = Date.from(Instant.now()) ;
+        return getClaims(token).getExpiration().before(today);
+    }
+
+    public boolean isTokenValid(String token, String username){
+        String subject = getSubject(token);
+        return subject.equals(username) && !isTokenExpired(token) ;
+
+
     }
 
 
